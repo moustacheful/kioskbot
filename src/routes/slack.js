1f0ccca771a6846c544dc714d47e9948c5eef694
Daniel Acuña <suchious@gmail.com>
2016-10-10 20:03:20 -0300

    Slack auth

--- src/routes/slack.js
+++ src/routes/slack.js
@@ -4,16 +4,32 @@ import _ from 'lodash';
 import SlackMiddleware from 'src/middleware/slack';
 import redis from 'src/lib/redis';
 import kiosk from 'src/lib/kiosk-service';
+import request from 'request-promise';
 
 const authRouter = Router({ prefix: '/slack/auth' });
 
 authRouter.use(bodyParser());
 
-authRouter.get('/callback', async (ctx) => {
-	ctx.body = {
-		query: ctx.query,
-		body: ctx.request.body,
-	}
+authRouter.get('/', async (ctx) => {
+	const scope = [
+		'commands'
+	];
+
+	ctx.redirect(`https://slack.com/oauth/authorize?scope=${scope.split(',')}'&client_id=${process.env.SLACK_CLIENT_ID}`);
+})
+
+authRouter.get('slack-callback', '/callback', async (ctx) => {
+	const token = await request('https://slack.com/api/oauth.access', {
+		method: 'POST',
+		json: true,
+		body: {
+			client_id: process.env.SLACK_CLIENT_ID,
+			client_secret: process.env.SLACK_CLIENT_SECRET,
+			redirect_uri: authRouter.url('slack-callback'),
+			code: ctx.query.code,
+		}
+	});
+	ctx.body = token;
 });
 
 const router = Router({ prefix: '/slack' });
