b15e283fdec088922e598626b09911b177e39e42
Daniel Acuña <dacuna@acid.cl>
2016-11-30 17:42:45 -0300

    Add newrelic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ silent: true });
+if (process.env.NODE_ENV !== 'development') require('newrelic');
 require('babel-register');
 require('babel-polyfill');
 require('app-module-path').addPath(process.cwd());
