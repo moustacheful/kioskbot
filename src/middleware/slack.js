1493139ab6c263b3fda23b8b30eaed06bca4d3a3
Daniel Acuña <suchious@gmail.com>
2017-05-23 23:44:01 -0400

    Errors will now not propagate their http status, use 200 instead for slack changes

--- src/middleware/slack.js
+++ src/middleware/slack.js
@@ -17,37 +17,37 @@ export const parse = async (ctx, next) => {
 	// Pass the state for the rest of the application
 	ctx.state.slack = incoming;
 	await next();
-}
+};
 
 export const secure = async (ctx, next) => {
-	if (ctx.state.slack.token !== process.env.SLACK_TOKEN){
+	if (ctx.state.slack.token !== process.env.SLACK_TOKEN) {
 		ctx.throw('Not allowed.', 401);
 	}
 	await next();
-}
+};
 
 export const user = async (ctx, next) => {
 	ctx.state.user = await User.findOneOrUpsertFromSlack(ctx.state.slack.user);
 	await next();
-}
+};
 
 export const errorHandler = async (ctx, next) => {
 	try {
 		await next();
 	} catch (error) {
 		// Handle errors in slack format.
-		if (error.status) ctx.status = error.status;
-		const template = error.status && error.status == 200 ? '<%= message %>' : ':boom: <%= message %> :fire:';
+		const template = ':boom: <%= message %>';
 
+		ctx.status = 200;
 		ctx.body = {
 			text: _.template(template)({ message: error.message || error }),
-			stack: error.stack && process.env.DEBUG ? error.stack : null
+			stack: error.stack && process.env.DEBUG ? error.stack : null,
 		};
 
 		console.error(error.message);
 		console.error(error.stack);
 	}
-}
+};
 
 export const formatResponse = async (ctx, next) => {
 	await next();
@@ -56,9 +56,9 @@ export const formatResponse = async (ctx, next) => {
 	ctx.body = {
 		mrkdwn: true,
 		response_type: 'ephemeral',
-		...ctx.body
-	}
-}
+		...ctx.body,
+	};
+};
 
 // Export as default the sequence of middleware
 export default [formatResponse, errorHandler, parse, secure, user];
